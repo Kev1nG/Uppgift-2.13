5a800cff8282e4350bf029d373230cb8a9334c7b
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-16 14:04:54 +0200

    Add project files.

---
+++ Uppgift 2.13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace uppgift2._13
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            int Fabian = 10000;
+            int Hashim = 12000;
+            int Abel = 2000;
+            int medellönen = Fabian + Hashim + Abel / 3;
+            int total = 24000;
+            Console.WriteLine("Medellönen blir " + medellönen + " eftersom all lön tillsamans blir " + total); //duktig
+            Console.ReadLine();
+        }
+    }
+}
