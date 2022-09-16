fd2882b4e4b6c65272136407fd60bfff3eac2dce
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-16 14:06:20 +0200

    så nu har jag skapat lite lön och räknat ut medelvärdet på denna lön så nu blir Fabian, Hashim och Abel glada.

--- Uppgift 2.13/Program.cs
+++ Uppgift 2.13/Program.cs
@@ -8,10 +8,10 @@ namespace uppgift2._13
             int Fabian = 10000;
             int Hashim = 12000;
             int Abel = 2000;
-            int medellönen = Fabian + Hashim + Abel / 3;
+            int medellönen = Fabian + Hashim + Abel / 3; //nice
             int total = 24000;
             Console.WriteLine("Medellönen blir " + medellönen + " eftersom all lön tillsamans blir " + total); //duktig
-            Console.ReadLine();
+            Console.ReadLine(); 
         }
     }
 }
